4856b33f2fb586bb9b5b3517097b5821ca2656c8
Marco <marco.monster@gmail.com>
2023-04-19 20:49:22 +0200

    anti-aliasing enabled

--- desktop/src/com/monstrous/pixelwar/DesktopLauncher.java
+++ desktop/src/com/monstrous/pixelwar/DesktopLauncher.java
@@ -9,10 +9,13 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		//config.setOpenGLEmulation(Lwjgl3ApplicationConfiguration.GLEmulation.GL30, 3, 3);
+
 		config.setForegroundFPS(60);
 		config.setTitle(Settings.title);
 		config.setWindowedMode(1200, 600);
 		config.setWindowIcon("icons/tank.png");
+		config.setBackBufferConfig(8, 8, 8, 8,16, 0, 4);		// anti-aliasing 4 samples
+
 		new Lwjgl3Application(new Main(), config);
 	}
 }
